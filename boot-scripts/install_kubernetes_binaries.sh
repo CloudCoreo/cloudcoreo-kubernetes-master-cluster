c6c81aa423f36b68e5bd15bada071fff0e21b1bc
Paul Allen <pauldallen@gmail.com>
2017-09-17 20:34:05 -0700

    run get-kub-binaries if it exists

--- boot-scripts/install_kubernetes_binaries.sh
+++ boot-scripts/install_kubernetes_binaries.sh
@@ -20,10 +20,10 @@ mkdir -p "$kube_dir/build"
     tar xzvf "kubernetes.tar.gz"
 
     if [ -f "kubernetes/cluster/get-kube-binaries.sh" ]; then
-	( sh -x "kubernetes/cluster/get-kube-binaries.sh" )
+	( echo yes | sh -x "kubernetes/cluster/get-kube-binaries.sh" )
     fi
 
     tar xzvf kubernetes/server/kubernetes-server-linux-amd64.tar.gz -C "$kube_dir/build"
     cp $kube_dir/build/kubernetes/server/bin/* $kube_dir/
-    rm -rf $kube_dir/build
+    #rm -rf $kube_dir/build
 )
